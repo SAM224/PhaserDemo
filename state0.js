be60ae8b17609d6163d38a94aba03a04e6f1ef0b
Shady A. Abdulhamid <SAMCS224@hotmail.com>
2017-09-14 21:17:46 +0200

    add camera and physics tricks (camera follow player, world bounds and player direction).

--- state0.js
+++ state0.js
@@ -5,22 +5,36 @@ demo.state0 = function(){};
 demo.state0.prototype = {
     preload: function(){
         game.load.image('pikachu','assets/sprites/pikachu.png');
+        game.load.image('tree', 'assets/backgrounds/treeBG.png')
     },
     create: function(){
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor = '#880088';
         console.log('state0');
         addChangeStateEventListeners();
+        game.world.setBounds(0, 0, 2813, 1000);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        var bg = game.add.sprite(0,0,'tree');
+
 
         pikachu = game.add.sprite(centerX,centerY,'pikachu');
         pikachu.anchor.setTo(0.5,0.5);
+        pikachu.scale.setTo(0.7,0.7);
+        game.physics.enable(pikachu);
+        pikachu.body.collideWorldBounds = true;
+
+        game.camera.follow(pikachu);
+        game.camera.deadzone = new Phaser.Rectangle(centerX-300,0,600,1000);
+
     },
     update: function(){
         // Horizontal Movement
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            pikachu.scale.setTo(-0.7,0.7);
             pikachu.x += speed;
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            pikachu.scale.setTo(0.7,0.7);
             pikachu.x -= speed;
         }
         // Vertical Movement
@@ -29,6 +43,9 @@ demo.state0.prototype = {
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
             pikachu.y -= speed;
+            if(pikachu.y < 395){
+                pikachu.y = 395;
+            }
         }
     }
 };
